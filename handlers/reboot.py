15521488d6354b070659914a1ed4e0ea4de3f88d
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-13 13:04:17 +0700

    miss

--- handlers/reboot.py
+++ handlers/reboot.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
 from handlers import check_heroku
+from config import OWNER_ID
 
-@Client.on_message(filters.command('rebootmusic') & filters.user(1249591948))
+@Client.on_message(filters.command('rebootmusic') & filters.user(OWNER_ID))
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_text("[Asuna Music] - Restarting")
