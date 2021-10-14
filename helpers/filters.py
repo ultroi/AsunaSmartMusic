fdeb8d9ecd974cb038d22fd68adec63b79f9de6a
ultroi <86416271+ultroi@users.noreply.github.com>
2021-10-14 20:28:16 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -8,7 +8,7 @@ other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.fo
 other_filters2 = (
     filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 )
-sudo_only = filters.user(SUDO_USERS)
+
 
 
 def command(commands: Union[str, List[str]]):
